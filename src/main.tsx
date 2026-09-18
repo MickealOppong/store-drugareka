92503dab2652703cd9abb303895a46848115f52b
mike oppong <mikeoppong@Mike.local>
2026-09-18 16:23:41 +0200

    v3

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App.tsx';
-import './css/index.css';
+//import './css/index.css';
 import './i18n.ts';
 import { store } from './store.ts';
 
